755f03371f54030b7ac3ae978cd32f875d1280bb
Katerina Ushakova <66923824+Katerina-Ushakova@users.noreply.github.com>
2020-06-21 23:27:59 +0300

    Coffee Machine

---
+++ Coffee Machine/Problems/Maximum element in a matrix/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int [][] twoDimArray = new int [n][m];
+        for (int i = 0; i <=n; i++) {
+            for (int j = 0; i)
+            twoDimArray[i] = scanner.nextInt();
+        }
+    }
+}
